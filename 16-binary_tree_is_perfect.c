c1534aad4799ca8ef94b567d11fe75a87ee3bd90
amigaye <mulerami9@gmail.com>
2024-02-02 05:24:17 -0800

    binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,100 +1,77 @@
-#include <stdio.h>
 #include "binary_trees.h"
 
-size_t get_binary_tree_height(const binary_tree_t *tree);
-int binary_tree_balance(const binary_tree_t *tree);
-int binary_tree_check_full(const binary_tree_t *tree);
-int binary_tree_is_full(const binary_tree_t *tree);
+size_t binary_tree_height2(const binary_tree_t *tree);
 
 /**
- * binary_tree_is_perfect - Checks if binary tree is perfect
- * @tree: Pointer to the root node of the tree to check
- * Return: 0 if tree is NULL, Otherwise 1 if it is perfect
+ *binary_tree_is_perfect - function
+ *@tree: pointer to the root node of the tree to check
+ *Return: if a binary tree is perfect 1 else 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	char balanced = (binary_tree_balance(tree) == 0) ? 1 : 0;
-	int full = binary_tree_is_full(tree);
+	size_t conf = 0, conf2 = 2, i = 0;
 
-	if (!tree)
-	{
+	if (tree == NULL)
 		return (0);
-	}
-	return (balanced && full);
-}
-
-/**
- * binary_tree_is_full - Checks if binary tree is full
- * @tree: Pointer to the root node to check if tree is full
- * Return: 0 if tree is NULL, Otherwise 1 if tree is full
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	int not_full = 0;
-
-	if (!tree)
-	{
-		return (0);
-	}
-	not_full = binary_tree_check_full(tree);
-	return ((not_full) ? 0 : 1);
-}
-
-/**
- * binary_tree_check_full - Checks the non full nodes
- * @tree: Pointer to the root node of binary tree to check
- * Return: The number of nodes that are not full
- */
-int binary_tree_check_full(const binary_tree_t *tree)
-{
-	size_t not_full = 0;
-
-	if (!tree)
-	{
-		return (0);
-	}
-	if ((tree->right && !tree->left) || (tree->left && !tree->right))
-	{
+	conf = binary_tree_height(tree);
+	for (i = 1; i < conf; i++)
+		conf2 = conf2 * 2;
+	if (binary_tree_leaves(tree) == conf2)
 		return (1);
-	}
-	not_full += binary_tree_check_full(tree->left);
-	not_full += binary_tree_check_full(tree->right);
-	return (not_full);
+	return (0);
 }
 
 /**
- * binary_tree_balance - Returns the balance factor of the tree
- * @tree: Pointer to the root node to calculate balance factor
- * Return: The balance factor of the tree
+ * binary_tree_leaves - function
+ *@tree: pointer to tree
+ *Return: number of leaves
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_h = 0, right_h = 0;
+	int cont1 = 0;
 
-	if (!tree)
-	{
+	if (tree == NULL)
 		return (0);
-	}
-	left_h = get_binary_tree_height(tree->left);
-	right_h = get_binary_tree_height(tree->right);
-	return (left_h - right_h);
+	if (tree->left == NULL && tree->right == NULL)
+		return (cont1 + 1);
+	if (tree->left != NULL)
+		cont1 += binary_tree_leaves(tree->left);
+	if (tree->right != NULL)
+		cont1 += binary_tree_leaves(tree->right);
+	return (cont1);
+}
+
+
+/**
+ * binary_tree_height2 - function
+ *@tree: pointer to tree
+ *Return: height
+ */
+
+size_t binary_tree_height2(const binary_tree_t *tree)
+{
+	int cont1 = 0, cont2 = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		cont1 += binary_tree_height2(tree->left);
+	if (tree->right != NULL)
+		cont2 += binary_tree_height2(tree->right);
+	if (cont1 > cont2)
+		return (cont1 + 1);
+	return (cont2 + 1);
 }
 
 /**
- * get_binary_tree_height - Counts the height of binary tree
- * @tree: Pointer to the root node to get height from
- * Return: The height of the tree
+ * binary_tree_height - function
+ *@tree: pointer to tree
+ *Return: height
  */
-size_t get_binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_h = 0;
-	size_t right_h = 0;
 
-	if (!tree)
-	{
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
 		return (0);
-	}
-	left_h += (tree) ? 1 + get_binary_tree_height(tree->left) : 0;
-	right_h += (tree) ? 1 + get_binary_tree_height(tree->right) : 0;
-	return ((right_h > left_h) ? right_h : left_h);
+	return ((binary_tree_height2(tree)) - 1);
 }
