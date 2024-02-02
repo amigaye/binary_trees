5ad2092e1a54bab45f67ff111b8b0fcce9628f8c
amigaye <mulerami9@gmail.com>
2024-02-02 03:25:09 -0800

    binary trees

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+size_t binary_tree_height2(const binary_tree_t *tree);
+
+/**
+ * binary_tree_height2 - function
+ *@tree: tree
+ *Return: height
+ */
+
+size_t binary_tree_height2(const binary_tree_t *tree)
+{
+int cont1 = 0, cont2 = 0;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left != NULL)
+cont1 += binary_tree_height2(tree->left);
+
+if (tree->right != NULL)
+cont2 += binary_tree_height2(tree->right);
+
+if (cont1 > cont2)
+return (cont1 + 1);
+return (cont2 + 1);
+}
+
+/**
+ * binary_tree_height - function
+ *@tree: tree
+ *Return: height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return ((binary_tree_height2(tree)) - 1);
+}
